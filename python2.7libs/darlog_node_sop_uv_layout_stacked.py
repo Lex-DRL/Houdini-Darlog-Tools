da7883df00f614dbdf0f1b11078d1848c1ed1dd1
Lex Darlog (DRL) <drl.i3x@gmail.com>
2023-02-03 16:42:58 +0400

    `sop_uvLayoutStacked`: update with the new "slash/split/meta/parm" approach instead of more resource-intensive json

--- python2.7libs/darlog_node_sop_uv_layout_stacked.py
+++ python2.7libs/darlog_node_sop_uv_layout_stacked.py
@@ -7,36 +7,27 @@ __author__ = 'Lex Darlog (DRL)'
 
 import hou
 
-from collections import OrderedDict
-from json import dumps
-
-from darlog_hou.attributes import CommonAttribsValidator, test_name_p_reserved
-from darlog_hou.errors import catch_error_message
+from darlog_hou.attributes import AttribFuncsPerGeo, test_name_p_reserved
+from darlog_hou.errors import catch_and_return_error_message, sop_input_geo
+from darlog_hou.py23 import str_format as _format
 
 
 _sizes = (2, 3, 4)
 
 
-def json_str(
-	node,  # type: hou.SopNode
-	uv_attr,  # type: str
-	out_uv_attr,  # type: str
+@catch_and_return_error_message(format=':{}')
+def test_in_out_uv_attrs(
+	node_with_input,  # type: hou.SopNode
+	uv_attr_nm,  # type: str
+	out_uv_attr_nm,  # type: str
 ):
-	def _main():
-		CommonAttribsValidator(
-			node
-		).test_uv(
-			uv_attr,
-			sizes=_sizes, allow_pt=False
-		)
-		test_name_p_reserved(out_uv_attr, 'out-UV')
+	uv_attr_nm = test_name_p_reserved(uv_attr_nm, 'input UV', split_by_spaces=True)
+	out_uv_attr_nm = test_name_p_reserved(out_uv_attr_nm, 'output UV', split_by_spaces=True)
 
-	caught_error, msg = catch_error_message(_main, '')
-
-	return dumps(
-		OrderedDict([
-			('is_error', caught_error is not None),
-			('error', msg),
-		]),
-		# indent=2,
+	in_geo = sop_input_geo(node_with_input, 0)
+	uv_attr = AttribFuncsPerGeo(in_geo, hou.attribType.Vertex).attr_test(
+		uv_attr_nm, data_types=hou.attribData.Float, sizes=_sizes,
+		error_attr_nice_nm='input UV'
 	)
+
+	return _format('{}/{}/{}', uv_attr.name(), uv_attr.size(), out_uv_attr_nm)
