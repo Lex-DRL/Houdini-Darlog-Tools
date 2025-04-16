562ccfd0d7a7a24a08fd16d934082715e8e69324
Lex Darlog (DRL) <3897975+Lex-DRL@users.noreply.github.com>
2025-04-17 03:44:51 +0500

    `sop_multiKineFX_foreach`

---
+++ python2.7libs/darlog_node_sop_multi_kinefx.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+"""
+Python code for various `multiKineFX*` SOP nodes.
+"""
+
+__author__ = 'Lex Darlog (DRL)'
+
+import hou
+
+from darlog_hou.attributes_2 import find_verify, SPECIFIER_PRIM_STR, AttributeErrorABC
+
+char_type_attr_nm = "character_type"
+fbx_path_attr_nm = "fbx_path"
+
+required_prim_str_attribs = (char_type_attr_nm, fbx_path_attr_nm)
+
+
+def verify_input(asset_node: hou.SopNode, python_node_geo: hou.Geometry):
+	"""Ensure that input has primitive attributes essential for all `multiKineFX*` nodes."""
+	for attr_nm in required_prim_str_attribs:
+		try:
+			find_verify(attr_nm, SPECIFIER_PRIM_STR, node=asset_node, geo=python_node_geo, geo_from_input=0, node_in_error=False)
+		except AttributeErrorABC as e:
+			raise hou.NodeError(e.message)
+			return
