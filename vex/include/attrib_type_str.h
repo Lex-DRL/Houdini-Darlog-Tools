03a80b92407584f05a0af8935121c179ddb1ac03
Lex Darlog (DRL) <3897975+Lex-DRL@users.noreply.github.com>
2025-05-30 23:49:59 +0500

    + tabs

--- vex/include/attrib_type_str.h
+++ vex/include/attrib_type_str.h
@@ -17,31 +17,31 @@
 
 
 function string attrib_type_str(int type_id, size) {
-    string names[] = {"integer", "float", "string", "integers-array", "floats-array", "strings-array", "dictionary", "dictionaries-array"};
-    if (type_id < 0 || type_id > 7) {
-        return "unknown";
-    }
-    if (type_id == 1 && size > 1) {
-        return sprintf("vector%d", size);
-    }
-    if (type_id == 4 && size > 1) {
-        return sprintf("vector%d-array", size);
-    }
-    return names[type_id];
+	string names[] = {"integer", "float", "string", "integers-array", "floats-array", "strings-array", "dictionary", "dictionaries-array"};
+	if (type_id < 0 || type_id > 7) {
+		return "unknown";
+	}
+	if (type_id == 1 && size > 1) {
+		return sprintf("vector%d", size);
+	}
+	if (type_id == 4 && size > 1) {
+		return sprintf("vector%d-array", size);
+	}
+	return names[type_id];
 }
 
 function string attrib_type_Capital_str(int type_id, size) {
-    string names[] = {"Integer", "Float", "String", "Integers-array", "Floats-array", "Strings-array", "Dictionary", "Dictionaries-array"};
-    if (type_id < 0 || type_id > 7) {
-        return "Unknown";
-    }
-    if (type_id == 1 && size > 1) {
-        return sprintf("Vector%d", size);
-    }
-    if (type_id == 4 && size > 1) {
-        return sprintf("Vector%d-array", size);
-    }
-    return names[type_id];
+	string names[] = {"Integer", "Float", "String", "Integers-array", "Floats-array", "Strings-array", "Dictionary", "Dictionaries-array"};
+	if (type_id < 0 || type_id > 7) {
+		return "Unknown";
+	}
+	if (type_id == 1 && size > 1) {
+		return sprintf("Vector%d", size);
+	}
+	if (type_id == 4 && size > 1) {
+		return sprintf("Vector%d-array", size);
+	}
+	return names[type_id];
 }
 
 #endif
